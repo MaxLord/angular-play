03738f99954d3b94f0769e3503edebc831895ad8
MaxLord <github@maxlord.ru>
2013-01-20 22:00:04 +0400

    beauty

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,6 @@ angular.module('playground', []).
     config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/pascal', {templateUrl: 'partials/pascal.html',   controller: FirstCtrl}).
-        when('/index/:Id', {templateUrl: 'partials/index.html', controller: FirstCtrl}).
+        when('/index', {templateUrl: 'partials/index.html', controller: FirstCtrl}).
         otherwise({redirectTo: '/index'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,10 @@
 /* Controllers */
 
 function FirstCtrl($scope) {
+    //$scope.atHome=true;
+    $scope.atHome=function () {
+        return(true);
+    };
     $scope.test = function (i, j) {
         if (i > 0 && j > 0) {
             return $scope.test(i, j - 1) + $scope.test(i - 1, j);
